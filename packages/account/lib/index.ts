1e51e656590d1691aacf3ff78872d04bbb6c5797
yerramreddyuday <uday.prasad@dojima.network>
2022-02-16 14:11:17 +0530

    Changed create account function to class

--- packages/account/lib/index.ts
+++ packages/account/lib/index.ts
@@ -1,1 +1,6 @@
-//entry point
+import { CreateAccount } from "./create_account";
+import * as bip39 from 'bip39'
+
+let mnemonic = bip39.generateMnemonic()
+
+export const account = new CreateAccount(mnemonic)
