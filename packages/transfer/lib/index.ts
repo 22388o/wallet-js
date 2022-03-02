9307352ee0321a91583fe1e0d09eb0ad901e84bf
bhaagiKenpachi <bhagath.reddy@dojima.network>
2022-03-02 13:57:43 +0530

    dev:pub: transfer package

--- packages/transfer/lib/index.ts
+++ packages/transfer/lib/index.ts
@@ -1,11 +1,11 @@
 import ArweaveChain from "./chains/arweave";
 import BitcoinChain from "./chains/bitcoin";
 import EthereumChain from "./chains/ethereum";
-import PolkadotChain from "./chains/polkadot";
+// import PolkadotChain from "./chains/polkadot";
 import SolanaChain from "./chains/solana";
 import ArweaveInitialise from "./utils.ts/arweave_initialise";
 import EthereumWeb3 from "./utils.ts/ethereum_web3";
-import PolkadotApi from "./utils.ts/polkadot_api";
+// import PolkadotApi from "./utils.ts/polkadot_api";
 import SolanaConnection from "./utils.ts/solana_connection";
 
 export {
@@ -13,9 +13,9 @@ export {
   ArweaveChain,
   EthereumChain,
   BitcoinChain,
-  PolkadotChain,
+  // PolkadotChain,
   ArweaveInitialise,
   EthereumWeb3,
-  PolkadotApi,
+  // PolkadotApi,
   SolanaConnection,
 };
