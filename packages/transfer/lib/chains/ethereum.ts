8314ea8bdcfb189304fb6e410a813aa905b620fe
yerramreddyuday <uday.prasad@dojima.network>
2022-03-03 22:34:24 +0530

    dev: mod: Modified ethereum chain functionality by using 'super' constructor and used ethers package

--- packages/transfer/lib/chains/ethereum.ts
+++ packages/transfer/lib/chains/ethereum.ts
@@ -1,44 +1,52 @@
 import Web3 from "web3";
-import Wallet from "ethereumjs-wallet";
+import * as ethers from "ethers";
+import { NetworkType } from "@dojima-wallet/types/dist/lib/network";
+import { EthereumWeb3 } from "lib";
 
-export default class EthereumChain {
-  _wallet: Wallet;
-  _web3: Web3;
-  pubKey: string;
-  pvtKey: string;
-  constructor(web3: Web3, wallet: Wallet) {
-    this._web3 = web3;
-    this._wallet = wallet;
-    this.pubKey = wallet.getAddressString();
-    this.pvtKey = wallet.getPrivateKeyString();
+export default class EthereumChain extends EthereumWeb3 {
+  _pubKey: string;
+  _pvtKey: string;
+  constructor(mnemonic: string, network: NetworkType) {
+    super(network);
+
+    // Wallet details
+    const wallet = ethers.Wallet.fromMnemonic(mnemonic);
+    this._pubKey = wallet.address;
+    this._pvtKey = wallet.privateKey;
+    // console.log('Key details : ', this._pvtKey);
+    // console.log('Address details : ', this._pubKey);
   }
 
-  async getBalance() {
-    const gweiBalance = await this._web3.eth.getBalance(this.pubKey);
-    // console.log("Balance in gwei is : ", gweiBalance); // Results balance in gwei, 1 eth = 10^9 gwei(1,000,000,000)
+  async getBalance(web3: Web3) {
+    const gweiBalance = await web3.eth.getBalance(this._pubKey);
+    // console.log('Balance in gwei is : ', gweiBalance);     // Results balance in gwei, 1 eth = 10^9 gwei(1,000,000,000)
 
-    const ethBalance = this._web3.utils.fromWei(gweiBalance);
-    // console.log("Balance in Eth is : ", ethBalance); // Results balance in gwei, 1 eth = 10^9 gwei(1,000,000,000)
+    const ethBalance = web3.utils.fromWei(gweiBalance);
+    // console.log('Balance in Eth is : ', ethBalance);     // Results balance in gwei, 1 eth = 10^9 gwei(1,000,000,000)
 
     return ethBalance;
   }
 
-  async createTransactionAndSend(toAddress: string, amount: number) {
-    const transaction = await this._web3.eth.accounts.signTransaction(
+  async createTransactionAndSend(
+    toAddress: string,
+    amount: number,
+    web3: Web3
+  ) {
+    const transaction = await web3.eth.accounts.signTransaction(
       {
-        from: this.pubKey,
+        from: this._pubKey,
         to: toAddress,
-        value: this._web3.utils.toWei(amount.toString(), "ether"), // Amount in Eth, 1 eth = 10^9 gwei(1,000,000,000)
+        value: web3.utils.toWei(amount.toString(), "ether"), // Amount in Eth, 1 eth = 10^9 gwei(1,000,000,000)
         gas: 21000, // Minimum / base gas fee is 21,000
       },
-      this.pvtKey
+      this._pvtKey
     );
-    // console.log("Transaction : ", transaction);
+    // console.log('Transaction : ', transaction);
 
-    const transactionResult = await this._web3.eth.sendSignedTransaction(
+    const transactionResult = await web3.eth.sendSignedTransaction(
       transaction.rawTransaction as string
     );
-    // console.log("Transaction details : ", transactionResult);
+    // console.log('Transaction details : ', transactionResult);
 
     return {
       transaction,
