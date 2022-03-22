124d54673853746b17d264d5113389472f97dbff
yerramreddyuday <uday.prasad@dojima.network>
2022-03-22 17:21:14 +0530

    dev: mod: Generate Bitcoin transactions data in history-tx package

--- packages/history-tx/lib/index.ts
+++ packages/history-tx/lib/index.ts
@@ -1,3 +1,4 @@
 import http, { get, put, post } from "./utils/http";
+import BitcoinTransactions from "./bitcoin";
 
-export { http, get, put, post };
+export { http, get, put, post, BitcoinTransactions };
